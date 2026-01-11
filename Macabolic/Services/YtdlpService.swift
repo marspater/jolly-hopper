69fb0610a0f9e1e66e51f4dd4ac1d0e6ab9d9069
alinuxpengui <alinuxpengui@users.noreply.github.com>
2026-01-12 01:40:54 +0300

    Fix version string to 1.2.6 in all files

--- Macabolic/Services/YtdlpService.swift
+++ Macabolic/Services/YtdlpService.swift
@@ -10,6 +10,7 @@ class YtdlpService: ObservableObject {
     
     private var ytdlpPath: URL?
     private var ffmpegPath: URL?
+    private let localVersion = "1.2.6"
     private let bundledYtdlpName = "yt-dlp_macos"
     
     init() {
