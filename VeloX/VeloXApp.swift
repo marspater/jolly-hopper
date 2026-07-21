9583d060ffe9de2f92e6a44b501d7c62ddf5555b
marspater <oleh.bodnar25@gmail.com>
2026-07-21 05:29:56 +0200

    fix: explicit CFBundleIconFile in Info.plist & default regular activation policy for Dock visibility

--- VeloX/VeloXApp.swift
+++ VeloX/VeloXApp.swift
@@ -77,6 +77,9 @@ struct VeloXApp: App {
                         window.close()
                     }
                 }
+            } else {
+                // Ensure Dock icon is visible
+                NSApp.setActivationPolicy(.regular)
             }
         }
     }
